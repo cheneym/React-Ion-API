22fd09d3b7b96434d20206b810031fb9d02a960e
Cheney Mao <cheneymao94@gmail.com>
2016-11-27 03:31:53 -0800

    (refactor) Remove client-hosting related server routes

--- server/config/middleware.js
+++ server/config/middleware.js
@@ -1,19 +1,14 @@
 const bodyParser = require('body-parser');
-const path = require('path');
 const morgan = require('morgan');
 const passport = require('passport');
 const session = require('express-session');
 const cookieParser = require('cookie-parser');
 
-module.exports = (app, express) => {
-  app.set('view engine', 'ejs');
-  app.set('views', path.join(__dirname, '/../../dist'));
+module.exports = (app) => {
   app.use(morgan('dev'));
   app.use(cookieParser());
   app.use(bodyParser.json());
   app.use(bodyParser.urlencoded({ extended: true }));
-  app.use(express.static(path.join(__dirname, '/../../dist')));
-  app.use('/editor', express.static(path.join(__dirname, '/../../dist')));
   app.use(session({ secret: 'password', resave: true, saveUninitialized: true }));
   app.use(passport.initialize());
   app.use(passport.session());
--- server/config/routes.js
+++ server/config/routes.js
@@ -1,7 +1,6 @@
 const userRouter = require('./routers/userRouter');
 const projectRouter = require('./routers/projectRouter');
 const authRouter = require('./routers/authRouter');
-const path = require('path');
 
 module.exports = (app) => {
   app.use((req, res, next) => {
@@ -10,11 +9,13 @@ module.exports = (app) => {
     res.header('Access-Control-Allow-Credentials', true);
     next();
   });
-  app.get('/editor/*', (req, res) => {
-    res.sendFile(path.join(__dirname, '../../dist/editor.html'));
-  });
   app.use('/api/user', userRouter);
   app.use('/api/project', projectRouter);
   app.use('/', authRouter);
-  app.get('/*', (req, res) => { res.status(404).send('404 Invalid Request'); });
+  app.get('/*', (req, res) => {
+    res.status(404).json({
+      errorCode: 404,
+      errorMessage: '404 Invalid Request',
+    });
+  });
 };
