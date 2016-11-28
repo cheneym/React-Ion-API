fa3c45501fd6ae55f80b1759251eb63776251c10
Cheney Mao <cheneymao94@gmail.com>
2016-11-27 23:11:03 -0800

    (feat) Add headers to allow get, post, put, delete, options methods

--- server/config/routes.js
+++ server/config/routes.js
@@ -6,6 +6,7 @@ module.exports = (app) => {
   app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', 'http://localhost:8080');
     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
     res.header('Access-Control-Allow-Credentials', true);
     next();
   });
