4574d8a44d36ed5d1d87e68873b55cb68c1ead00
Andrew <anxsaiyuki@gmail.com>
2016-11-30 16:07:37 -0800

    (feat) Able to produce carousel components

--- server/utils/componentHelper.js
+++ server/utils/componentHelper.js
@@ -7,6 +7,7 @@ const IMAGE_COMPONENT = 'Image';
 const LIST_COMPONENT = 'List';
 const RADIO_COMPONENT = 'Radio';
 const DROPDOWN_COMPONENT = 'DropDown';
+const CAROUSEL_COMPONENT = 'Carousels';
 
 const inlineComponent = (type) => {
   if (type === TEXT_COMPONENT ||
@@ -35,6 +36,9 @@ const getComponent = (tree) => {
       break;
     case IMAGE_COMPONENT:
       componentPath = filePath.IMAGE_TEMPLATE_PATH;
+      break;
+    case CAROUSEL_COMPONENT:
+      componentPath = filePath.CAROUSEL_TEMPLATE_PATH;
       break;
     default:
       break;
